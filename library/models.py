6a2df73b30a0ca89c7d4fd629fb9e07918859b64
ssani4 <ssani4@icloud.com>
2025-04-03 13:38:52 +0200

    task 5

--- library/models.py
+++ library/models.py
@@ -29,9 +29,36 @@ class Author(models.Model):
     def __str__(self):
         return self.name
 
+# Наш senior посмотрел на модель, что мы создали и предложил внести некоторые правки по улучшению модели.
+#
+# В модель Book добавьте дополнительные поля для большей информации о книге:
+#
+# Краткое описание: TextField, может быть пустым
+# Жанр: CharField, макс. длина 50, выборы из ['Fiction', 'Non-Fiction', 'Science Fiction', 'Fantasy', 'Mystery', 'Biography']
+# Кол-во страниц: может быть пустым, максимальное кол-во - 10000
+# Примените изменения в модели, попробуйте создать записи, не указывая некоторые поля. Обратите внимание на то, как отрабатывает система.
+
 class Book(models.Model):
+    GENRE_CHOICES = [
+        ('Fiction','Fiction'),
+        ('Non-Fiction','Non-Fiction'),
+        ('Science Fiction','Science Fiction'),
+        ('Fantasy','Fantasy'),
+        ('Mystery','Mystery'),
+        ('Biography','Biography'),
+
+    ]
     title = models.CharField(max_length=120)
     author = models.ForeignKey(Author, on_delete=models.PROTECT)
+    genre = models.CharField(max_length=50, choices=GENRE_CHOICES)
+    description = models.TextField(null=True, blank=True)
+    count_pages = models.PositiveSmallIntegerField( # 32000 symbols
+        validators=[
+            MinValueValidator(1),
+            MaxValueValidator(10000)
+        ],
+        null=True,
+        blank=True)
     public_date = models.DateField()
 
     def __str__(self):
