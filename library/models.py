cb85e0b0a8ae047c6260e544086d530753c3b302
Izzet <yunusovizzet6@gmail.com>
2025-04-03 11:39:49 +0200

    Task 2

--- library/models.py
+++ library/models.py
@@ -1,9 +1,25 @@
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 
 class Author(models.Model):
-    first_name = models.CharField(max_length=100) #first_name VARCHAR(100)
-    last_name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100) #first_name VARCHAR(100)
+    surname = models.CharField(max_length=100)
     birth_date = models.DateField()
+    profile = models.URLField(max_length=255, null=True, blank=True)
+    is_deleted = models.BooleanField(default=False)
+    rating = models.FloatField(
+        validators=[
+            MinValueValidator(1),
+            MaxValueValidator(10)
+        ],
+        default=0.0
+    )
+    deleted_at = models.DateTimeField(null=True, blank=True)
+
+
+    def __str__(self):
+        return self.name
+
 
 
 
